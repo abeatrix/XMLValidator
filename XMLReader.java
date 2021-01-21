6df3cbb1e7e4f01f6c6d3ee124dc6a9077c3e01e
Beatrix Woo <beatrixwho@gmail.com>
2021-01-21 14:00:51 -0800

    cleaned up

--- XMLReader.java
+++ XMLReader.java
@@ -1,18 +1,15 @@
 import java.io.FileInputStream;
 import java.io.InputStream;
 import javax.xml.stream.*;
-import javax.xml.xpath.*;
 
 public class XMLReader{
 
 	public static void main(String argv[]) throws Exception {
+        //
         XMLInputFactory inputFactory = XMLInputFactory.newInstance();
         InputStream in = new FileInputStream("./consumer.xml");
 
         XMLStreamReader streamReader = inputFactory.createXMLStreamReader(in);
-        streamReader.nextTag(); // Advance to "consumer" element
-        streamReader.nextTag(); // Advance to "consumerId" element
-        streamReader.nextTag(); // Advance to "consumerName" element
 
         System.out.println("Description:");
         while (streamReader.hasNext()) {
@@ -20,46 +17,46 @@ public class XMLReader{
                 switch (streamReader.getLocalName()) {
                 case "bankAccount": {
                     System.out.print(streamReader.getLocalName()+": ");
-                    streamReader.nextTag();
+                    streamReader.nextTag(); // Advance to "accountNumber" element
                     String acctNum = (String) streamReader.getElementText();
-                    streamReader.nextTag();
+                    streamReader.nextTag(); // Advance to "amount" element
                     String amount = (String) streamReader.getElementText();
-                    streamReader.nextTag();
-                    streamReader.nextTag();
+                    streamReader.nextTag(); // Advance to "description" element
+                    streamReader.nextTag(); // Advance to "accountType" element
                     String type = (String) streamReader.getElementText();
-                    streamReader.nextTag();
+                    streamReader.nextTag(); // Advance to "lenderName" element
                     String lender = (String) streamReader.getElementText();
                     System.out.println("bankAccount : bankAccountType: "+type+",bankName: "+lender+", amount: "+ amount);
                     break;
                 }
                 case "creditCardAccount": {
                     System.out.print(streamReader.getLocalName()+": ");
-                    streamReader.nextTag();
+                    streamReader.nextTag(); // Advance to "accountNumber" element
                     String acctNum = (String) streamReader.getElementText();
-                    streamReader.nextTag();
+                    streamReader.nextTag(); // Advance to "amount" element
                     String amount = (String) streamReader.getElementText();
-                    streamReader.nextTag();
-                    streamReader.nextTag();
+                    streamReader.nextTag(); // Advance to "description" element
+                    streamReader.nextTag(); // Advance to "accountType" element
                     String type = (String) streamReader.getElementText();
-                    streamReader.nextTag();
+                    streamReader.nextTag(); // Advance to "creditCardNumber" element
                     String cardNumber = (String) streamReader.getElementText();
-                    streamReader.nextTag();
+                    streamReader.nextTag(); // Advance to "expirationDate" element
                     String date = (String) streamReader.getElementText();
-                    streamReader.nextTag();
+                    streamReader.nextTag(); // Advance to "issuer" element
                     String issuer = (String) streamReader.getElementText();
                     System.out.println("creditCardNumber: "+cardNumber+",expirationDate: "+date+", Issuer: "+ issuer+", amount: "+amount);
                     break;
                 }
                 case "mortgageAccount": {
                     System.out.print(streamReader.getLocalName()+": ");
-                    streamReader.nextTag();
+                    streamReader.nextTag(); // Advance to "accountNumber" element
                     String acctNum = (String) streamReader.getElementText();
-                    streamReader.nextTag();
+                    streamReader.nextTag(); // Advance to "amount" element
                     String amount = (String) streamReader.getElementText();
-                    streamReader.nextTag();
-                    streamReader.nextTag();
+                    streamReader.nextTag(); // Advance to "description" element
+                    streamReader.nextTag(); // Advance to "accountType" element
                     String type = (String) streamReader.getElementText();
-                    streamReader.nextTag();
+                    streamReader.nextTag(); // Advance to "lenderName" element
                     String lender = (String) streamReader.getElementText();
                     System.out.println(streamReader.getLocalName()+": "+lender+", accountType: "+type+", amount: "+ amount);
                     break;
